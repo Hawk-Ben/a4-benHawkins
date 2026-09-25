8f767a8999e60900ab5c854174f8f030cc511983
Ben Hawkins <147286158+Hawk-Ben@users.noreply.github.com>
2026-09-25 10:56:20 -0400

    Porting to React, halfway there

--- public/js/main.js
+++ public/js/main.js
@@ -8,40 +8,6 @@ const editBrickForm = document.getElementById("brickEditForm")
 const editTitle = document.getElementById('editTitle')
 const editBody = document.getElementById('editBody')
 
-function displayBrick( brick ) {
-  if (userID && brick.userID !== userID) {
-    return
-  }
-  const brickElement = document.createElement( 'div' )
-
-  brickElement.dataset.id = brick.id
-  brickElement.classList.add( 'brick' )
-
-  const titleElement = document.createElement( 'h3' )
-  titleElement.textContent = brick.title
-
-  const bodyElement = document.createElement( 'p' )
-  bodyElement.textContent = brick.body
-
-  brickElement.appendChild( titleElement )
-  brickElement.appendChild( bodyElement )
-
-  brickElement.addEventListener( 'click', brickClicked )
-
-  const wall = document.getElementById("brickWall")
-  if (!wall) {
-    return
-  }
-
-  wall.appendChild( brickElement )
-  if (brick.parentID !== -1) {
-    parentBrick = bricks.find(parentBrick => Number(parentBrick.id) == brick.parentID)
-    parentBrickElement = document.querySelector(`.brick[data-id='${parentBrick.id}']`)
-    drawLine( brickElement, parentBrickElement )
-  }
-
-  console.log( 'brickElement:', brickElement )
-}
 
 async function loadBricks() {
   const response = await fetch( 'api/bricks' )
@@ -296,9 +262,9 @@ window.onload = function() {
 }
 
 //-----------------------------React code past this------------------------
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function brick({title, body, onBrickClick}){
+function Brick({title, body, onBrickClick}){
   return(
     <>
     <div className="brick" onClick={onBrickClick}>
@@ -310,14 +276,117 @@ function brick({title, body, onBrickClick}){
 }
 
 export default function Wall(){
-  const [selectedBrick, setSelectedBrick] = useState(-1)
+  const [bricks, setBricks] = useState([]);
+  const [selectedBrickID, setSelectedBrickID] = useState(null);
+  const [userID, setUserID] = useState(sessionStorage.getItem("brickWallUserID"));
 
-  function handleClick(i){
-    if (i == selectedBrick){
-      setSelectedBrick(-1)
-      //No bricks are yellow
+  
+  async function fetchBricks(){
+    const response = await fetch("/api/bricks");
+    const data = await response.json();
+
+    setBricks(data)
+  }
+
+  async function handleCreateBrick(title, body){
+
+    const newBrick = {
+      title: title,
+      body: body,
+      parentID: selectedBrickID,
+      userID: userID
+    };
+
+    const response = await fetch("/api/bricks", {
+      method: "POST",
+      headers: { 
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify(newBrick)
+    });
+
+    const savedBrick = await response.json();
+
+    setBricks(prev => [...prev, savedBrick]);
+
+  }
+
+  async function handleEditBrick(title, body){
+
+    if(!selectedBrickID){
+      return;
     }
+
+    const response = await fetch(
+      `/api/bricks/${selectedBrickID}`,
+      {
+        method: "PUT",
+        headers: { 
+          'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({
+          title,
+          body
+        })
+      }
+    );
+
+    const updatedBrick = await response.json();
+
+    setBricks(prev => 
+      prev.map(brick =>
+        brick._id === selectedBrickID
+          ? {...brick, title, body}
+          : brick
+      )
+    );
+  }
+
+  //NEXT STEP: EDIT BRICK FORM
+
+  function CreateBrickForm({ onCreate }){
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    function handleSubmit(event){
+      event.preventDefault();
+
+      onCreate(title, body);
+
+      setTitle("");
+      setBody("");
+    }
+
+    return(
+      <form onSubmit={handleSubmit}>
+
+        <input
+          value = {title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title of your brick..."
+        />
+
+        <input
+          value = {body}
+          onChange={(e) => setBody(e.target.value)}
+          placeholder="What will you write on the wall..."
+        />
+
+        <button type="submit">
+          Lay your brick  
+        </button>
+
+      </form>
+    );
+  }
+  
+  function handleBrickClick(id){
+    if (id == selectedBrickID){
+      setSelectedBrickID(null)
+      //No bricks are yellow
+    }else{
     setSelectedBrick(i)
+    }
     //Make the brick selected yellow
   }
 
@@ -326,7 +395,9 @@ export default function Wall(){
   //Push bricks to server
   //Edit bricks from text box
   //Create line when brick created with selected brick (should be able to reuse code)
-
+  useEffect(() => {
+      fetchBricks();
+    }, []);
   //WEB DESIGN ON RETURN
   return(
     <>
@@ -338,34 +409,20 @@ export default function Wall(){
     <p class="header">Click on a brick then lay a new brick to connect them</p>
 
     </div>
-    <div id="brickWall"></div>
-    <div class="underWall">
-      <form id='childBrickForm' class="childBrickForm">
-      <input type="text" id="childBrickTitle" name="title" placeholder="Title of your brick..." required></input>
-
-      <textarea
-        id="childBrickBody"
-        name="body"
-        placeholder="What will you write on the wall..."
-        required
-      ></textarea>
-
-      <button type="submit" id="childBrickButton">Lay your brick</button>
-    </form>
-
-    <form id='editForm' class="childBrickForm">
-      <input type="text" id="editTitle" name="title" placeholder="Select a brick to edit" required></input>
-
-      <textarea
-        id="editBody"
-        name="body"
-        placeholder="Edit the body here"
-        required
-      ></textarea>
-
-      <button type="submit" id="editButton">Edit the selected brick</button>
-    </form>
+    <div id="brickWall">
+      {bricks.map((brick) => (
+        <Brick
+          key={brick._id}
+          brick={brick}
+          selected={brick._id === selectedBrickID}
+          onClick={() => handleBrickClick(brick._id)}
+        />
+      ))}
     </div>
+
+    <CreateBrickForm onCreate={handleCreateBrick} />
+
+
     <svg id="connection"></svg>
 
   </body>
